bb2a44cdbb79d8414938acbc0bd4477c89689de8
knz13 <otaviomaya@gmail.com>
2022-07-31 17:02:26 -0300

    Adicionando componentes uteis e modificando banco de dados

--- laranja_radioativa/components/accordion.tsx
+++ laranja_radioativa/components/accordion.tsx
@@ -3,60 +3,67 @@ import React from 'react';
 import { useState } from 'react';
 import { StyleProp, StyleSheet, Text, TextProps, View, ViewStyle } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import Animated , {runOnJS, useAnimatedStyle, useSharedValue, withTiming} from 'react-native-reanimated';
+import Animated , {measure, runOnJS, runOnUI, useAnimatedRef, useAnimatedStyle, useDerivedValue, useSharedValue, withSpring, withTiming} from 'react-native-reanimated';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useEffect } from 'react';
+
 
 interface AccordionProps {
   headerStyle?:StyleProp<ViewStyle>,
   headerTitle:string,
   children?: React.ReactNode,
   contentStyle?:StyleProp<ViewStyle>,
-  maxSize?: number
+  duration?:number
 }
 
-export const Accordion = ({children,headerStyle,headerTitle,contentStyle,maxSize} : AccordionProps) => {
+export const Accordion = ({children,headerStyle,headerTitle,contentStyle,duration} : AccordionProps) => {
 
-  // Texto inicial
-  // clica no texto inicial e abre para mais informações
-  const [isOpen,setIsOpen] = useState(false);
-  const [isShown,setIsShown] = useState(false);
-  const animationHeight = useSharedValue(0);
+  const viewRef = useAnimatedRef<View>();
+  const isOpen = useSharedValue(false);
+  const height = useSharedValue(0);
+  const oldHeight = useSharedValue(0);
+  const progress = useDerivedValue(() => 
+    isOpen.value? duration? withTiming(1,{duration:duration}) : withTiming(1) : duration? withTiming(0,{duration:duration}) : withTiming(0)
+  )
+
 
   const style = useAnimatedStyle(() => {
+
+    if(oldHeight.value === 0){
+      return {
+        opacity: 0.1
+      }
+    }
+
     return {
-      height:animationHeight.value
+      height: height.value * progress.value + 0.1,
+      opacity: 1
     }
   })
 
-  const StopShowing = () => {
-    setIsShown(false);
-  }
-
-  useEffect(() => {
-    if(isOpen){
-      animationHeight.value = withTiming(maxSize? maxSize : 100,{duration:1000});
-    }
-    else {
-      animationHeight.value = withTiming(0,{duration:1000},() => runOnJS(StopShowing)());
-    }
-  },[isOpen])
-
-
   return <>
   <TouchableOpacity onPress={() => {
-    isOpen? setIsOpen(false) : setIsOpen(true); setIsShown(true)
+    isOpen.value = !isOpen.value
   }}>
-  <View style={[{borderWidth:1,alignItems:'center',borderRadius:5},headerStyle]}>
-    <Text style={{margin:10}}>{headerTitle}</Text>
-  </View>
+    <View style={[{borderWidth:1,alignItems:'center',borderRadius:5},headerStyle]}>
+      <Text style={{margin:10}}>{headerTitle}</Text>
+    </View>
   </TouchableOpacity>
-  {/* view de dentro */}
-  {isShown && (
-  <Animated.View style={[{borderWidth:1,width:'100%'},contentStyle,style]}>
-    {children}
-  </Animated.View>
-  )}
+  <Animated.View  style={[contentStyle,{overflow:'hidden',width:'100%'},style]} onLayout={() => {
+    if(height.value === 0 || height.value > oldHeight.value){
+      runOnUI(() => {
+        'worklet';
+        height.value = measure(viewRef).height
+        if(oldHeight.value === 0){
+          oldHeight.value = height.value;
+        }
+      })()
+    }
+    
+  }}>
+    <View ref={viewRef} style={{width:'100%'}}>
+      {children}
+    </View>
+  </Animated.View>  
   </>
 }
-
--- laranja_radioativa/styles.tsx
+++ laranja_radioativa/styles.tsx
@@ -3,9 +3,12 @@ import { StyleSheet } from "react-native";
 
 
 export const AppColors = {
-    white: 'rgb(255,255,255)'
-
-
+    white: 'rgb(255,255,255)',
+    amarelo_radioativo: '#f09137',
+    laranja_radioativo: '#cd5f00',
+    vermelho_saturado: '#a52728',
+    marrom: '#9a4800',
+    preto: '#000000'
 };
 
 
