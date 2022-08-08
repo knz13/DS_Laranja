34a8a5cb331cd5e63725a1c66b79b39b0c9d6fad
knz13 <otaviomaya@gmail.com>
2022-08-08 15:40:02 -0300

    Update tela_de_compendium.tsx

--- laranja_radioativa/src/TelaDeCompendium/tela_de_compendium.tsx
+++ laranja_radioativa/src/TelaDeCompendium/tela_de_compendium.tsx
@@ -1,25 +1,19 @@
 import {DBContext, Props} from "../geral";
 import React, { useState,Component, useContext, useEffect } from 'react';
-<<<<<<< Updated upstream
-import { StyleSheet,Switch, Text, View,Button, TextInput,TouchableOpacity, Pressable,Keyboard, TouchableHighlight, TouchableWithoutFeedback, ScrollView } from 'react-native';
-import { Styles } from "../styles";
-import { MainView } from "../components/MainView";
 
-
-=======
 import { StyleSheet,Switch, Text, View, TextInput,TouchableOpacity, Pressable,Keyboard, TouchableHighlight, TouchableWithoutFeedback, ScrollView } from 'react-native';
 import { AppColors, Styles } from "../styles";
 import { MainView } from "../components/MainView";
-import { CreationButton } from "../components/CreationButton";
+import { PageButton } from "../components/PageButton";
 import { FlatList } from "react-native-gesture-handler";
 import Button from "../components/Button";
 import { SafeAreaView } from "react-native-safe-area-context";
->>>>>>> Stashed changes
+
 
 
 //style dos buttons
 const styles1 = StyleSheet.create({button:{
-    backgroundColor:AppColors.laranja_radioativo,padding:20,marginHorizontal:8,marginVertical:16,borderRadius:6,width:300}
+    backgroundColor:AppColors.laranja_radioativo,marginHorizontal:8,marginVertical:8,borderRadius:6,width:300}
 })
 
 
@@ -27,19 +21,8 @@ export const TelaDeCompendium = () => {
     const item_types = ['Armor','Weapon','Adventuring Gear','Tools','Poisons','Potions and Oils','Wondrous Item','Other']
 
     return <MainView>
-            <ScrollView contentContainerStyle={{flexGrow:1, marginHorizontal:20}}>
-                {item_types.map(type => <CreationButton title={type} style={styles1.button} ></CreationButton>)}
+            <ScrollView contentContainerStyle={{flexGrow:1, marginHorizontal:20,marginTop:'20%'}}>
+                {item_types.map(type => <PageButton title={type} style={styles1.button} ></PageButton>)}
             </ScrollView>   
     </MainView>
 }
-
-
-
-    return <MainView>
-        <ScrollView style={{}}></ScrollView>
-    </MainView>
-}
-=======
-//Telas de cada Categoria
-//uso de HOOKS
->>>>>>> Stashed changes
