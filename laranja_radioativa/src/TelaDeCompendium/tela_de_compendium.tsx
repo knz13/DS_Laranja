d4842defa3a02191e863699d9c25c13285cdf5cf
Pedro Henrique Braga <98167728+Stain19@users.noreply.github.com>
2022-08-07 21:07:36 -0300

    Aplicando secoes dos items

--- laranja_radioativa/src/TelaDeCompendium/tela_de_compendium.tsx
+++ laranja_radioativa/src/TelaDeCompendium/tela_de_compendium.tsx
@@ -1,31 +1,45 @@
 import {DBContext, Props} from "../geral";
 import React, { useState,Component, useContext, useEffect } from 'react';
+<<<<<<< Updated upstream
 import { StyleSheet,Switch, Text, View,Button, TextInput,TouchableOpacity, Pressable,Keyboard, TouchableHighlight, TouchableWithoutFeedback, ScrollView } from 'react-native';
 import { Styles } from "../styles";
 import { MainView } from "../components/MainView";
 
 
+=======
+import { StyleSheet,Switch, Text, View, TextInput,TouchableOpacity, Pressable,Keyboard, TouchableHighlight, TouchableWithoutFeedback, ScrollView } from 'react-native';
+import { AppColors, Styles } from "../styles";
+import { MainView } from "../components/MainView";
+import { CreationButton } from "../components/CreationButton";
+import { FlatList } from "react-native-gesture-handler";
+import Button from "../components/Button";
+import { SafeAreaView } from "react-native-safe-area-context";
+>>>>>>> Stashed changes
 
 
+//style dos buttons
+const styles1 = StyleSheet.create({button:{
+    backgroundColor:AppColors.laranja_radioativo,padding:20,marginHorizontal:8,marginVertical:16,borderRadius:6,width:300}
+})
 
 
 export const TelaDeCompendium = () => {
+    const item_types = ['Armor','Weapon','Adventuring Gear','Tools','Poisons','Potions and Oils','Wondrous Item','Other']
 
-    const db = useContext(DBContext);
+    return <MainView>
+            <ScrollView contentContainerStyle={{flexGrow:1, marginHorizontal:20}}>
+                {item_types.map(type => <CreationButton title={type} style={styles1.button} ></CreationButton>)}
+            </ScrollView>   
+    </MainView>
+}
 
-    const [data,setData] = useState([] as Array<string>)
 
-    useEffect(() => {
-        db.readTransaction(tx => {
-            tx.executeSql('SELECT class_name FROM classes',[],(tx,result) => {
-                setData(result.rows._array);
-            });
-        },(err) => {
-            console.log(err)
-        })
-    })
 
     return <MainView>
         <ScrollView style={{}}></ScrollView>
     </MainView>
 }
+=======
+//Telas de cada Categoria
+//uso de HOOKS
+>>>>>>> Stashed changes
