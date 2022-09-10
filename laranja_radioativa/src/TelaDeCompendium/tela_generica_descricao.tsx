3c0117e87b37e92ed0bacfe4b6338ee320ab71cf
Pedro Henrique Braga <prometeuslbo@gmail.com>
2022-09-10 16:56:41 -0300

    Adicionando mudanças na tela de descrição dos items

--- laranja_radioativa/src/TelaDeCompendium/tela_generica_descricao.tsx
+++ laranja_radioativa/src/TelaDeCompendium/tela_generica_descricao.tsx
@@ -18,26 +18,26 @@ import { AppColors } from "../styles";
       alignSelf:'center',
       width:'100%',
       backgroundColor:AppColors.azul_escuro_fundo}}>
-      {prop.route.params.description != 'null' && 
+      {prop.route.params.description != null && 
       <View>
-      <Text style={{alignContent:'center',color:AppColors.azul,marginLeft: '10%',textAlign:'left',fontSize: 50,marginRight:'10%'}}>{prop.route.params.item_name}</Text>
-      <Text style={{alignContent:'center',color:AppColors.azul,marginLeft: '10%',textAlign:'left',fontSize: 20,marginRight:'10%',marginTop:'15%'}}>Description</Text>
-      <View style={{borderColor:AppColors.preto, borderWidth: 2, marginLeft: '10%',marginRight:'10%',marginBottom:'5%',marginTop:'2.5%'}}>
-        <Text style={{backgroundColor:AppColors.azul_escuro_extra, padding:'05%'}}> {prop.route.params.description}</Text>
+      <Text style={{alignContent:'center',color:AppColors.azul,marginLeft: '10%',textAlign:'left',fontSize: 50,marginRight:'10%',fontFamily:'inter'}}>{prop.route.params.item_name}</Text>
+      <Text style={{alignContent:'center',color:AppColors.azul,marginLeft: '10%',textAlign:'left',fontSize: 20,marginRight:'10%',marginTop:'15%',fontFamily:'inter'}}>Description</Text>
+      <View style={{backgroundColor:AppColors.azul_escuro_extra, borderColor:AppColors.preto, borderWidth: 2, marginLeft: '10%',marginRight:'10%',marginBottom:'5%',marginTop:'2.5%',borderRadius:15}}>
+        <Text style={{padding:'05%',color:AppColors.white,fontFamily:'inter'}}> {prop.route.params.description}</Text>
       </View>
       </View>}
       {prop.route.params['rarity'] && 
       <View>
-      <Text style={{alignContent:'center',color:AppColors.azul,marginLeft: '10%',textAlign:'left',fontSize: 20,marginRight:'10%',marginTop:'15%'}}>Rarity</Text>
-      <View style={{borderColor:AppColors.preto, borderWidth: 2, marginLeft: '10%',marginRight:'10%',marginBottom:'5%',marginTop:'2.5%'}}>
-        <Text style={{backgroundColor:AppColors.azul_escuro_extra, padding:'05%'}}> {prop.route.params.rarity}</Text>
+      <Text style={{alignContent:'center',color:AppColors.azul,marginLeft: '10%',textAlign:'left',fontSize: 20,marginRight:'10%',marginTop:'15%',fontFamily:'inter'}}>Rarity</Text>
+      <View style={{backgroundColor:AppColors.azul_escuro_extra, borderColor:AppColors.preto, borderWidth: 2, marginLeft: '10%',marginRight:'10%',marginBottom:'5%',marginTop:'2.5%',borderRadius:15}}>
+        <Text style={{padding:'05%',color:AppColors.white,fontFamily:'inter'}}> {prop.route.params.rarity}</Text>
       </View>
       </View>}
       {prop.route.params['price'] && 
       <View>
-      <Text style={{alignContent:'center',color:AppColors.azul,marginLeft: '10%',textAlign:'left',fontSize: 20,marginRight:'10%',marginTop:'15%'}}>Price</Text>
-      <View style={{borderColor:AppColors.preto, borderWidth: 2, marginLeft: '10%',marginRight:'10%',marginBottom:'5%',marginTop:'2.5%'}}>
-        <Text style={{backgroundColor:AppColors.azul_escuro_extra, padding:'05%'}}> {prop.route.params.price}</Text>
+      <Text style={{alignContent:'center',color:AppColors.azul,marginLeft: '10%',textAlign:'left',fontSize: 20,marginRight:'10%',marginTop:'15%',fontFamily:'inter'}}>Price</Text>
+      <View style={{backgroundColor:AppColors.azul_escuro_extra, borderColor:AppColors.preto, borderWidth: 2, marginLeft: '10%',marginRight:'10%',marginBottom:'5%',marginTop:'2.5%',borderRadius:15}}>
+        <Text style={{padding:'05%',color:AppColors.white,fontFamily:'inter'}}> {prop.route.params.price}</Text>
       </View>
       </View>}
       </ScrollView>
