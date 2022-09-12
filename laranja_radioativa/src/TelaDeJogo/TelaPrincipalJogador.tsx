6a491799391f1c2a358e41669559b72053eff048
knz13 <otaviomaya@gmail.com>
2022-09-12 16:51:57 -0300

    Update TelaPrincipalJogador.tsx

--- laranja_radioativa/src/TelaDeJogo/TelaPrincipalJogador.tsx
+++ laranja_radioativa/src/TelaDeJogo/TelaPrincipalJogador.tsx
@@ -9,6 +9,7 @@ import { MainTextInput } from "../components/MainTextInput";
 import Logout from './../components/logout'
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import App from "../../App";
 
 
 
@@ -16,16 +17,27 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 export const TelaPrincipalJogador = (props: NativeStackScreenProps<{}>) => {
     const navigation = useNavigation();
     const global = useContext(GlobalContext);
+    const [personagem,setPersonagem] = useState(null)
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            fetch(`https://dnd-party.herokuapp.com/database/gameplay/${props.route.params.char_id}`,{
+        if(personagem == null){
+          fetch(`https://dnd-party.herokuapp.com/database/character/${props.route.params.char_id}`,{
                 method:"GET",
                 headers:{
                     'x-access-token':global.token
                 }
             }).then(response => response.json()).then(json => {
-                console.log(JSON.stringify(json))
+                setPersonagem(JSON.parse(json['message']));
+            })
+        }
+        const interval = setInterval(() => {
+            fetch(`https://dnd-party.herokuapp.com/database/character/${props.route.params.char_id}`,{
+                method:"GET",
+                headers:{
+                    'x-access-token':global.token
+                }
+            }).then(response => response.json()).then(json => {
+                setPersonagem(JSON.parse(json['message']));
             })
         }, 5000);
         
@@ -42,5 +54,36 @@ export const TelaPrincipalJogador = (props: NativeStackScreenProps<{}>) => {
         </View>
       </TouchableOpacity>
     </View>
+    {personagem && <><PageButton title={`${personagem.nome}\nraça: ${personagem.race}\nclasse: ${personagem.classe}`} textStyle={{textAlign:'center'}} style={{backgroundColor:AppColors.azul_escuro_fundo,borderColor:AppColors.azul,borderWidth:1,width:'80%',marginVertical:'10%'}}>
+      <View style={{height:'100%',left: 10,position:'absolute',alignItems:'center',justifyContent:'center'}}>
+      <Text style={{color:'white',fontFamily:'inter',fontSize:20}}>{personagem.nivel}</Text>
+      </View>
+    </PageButton>
+    <View style={{width:'70%'}}>
+    <View style={{flexDirection:'row',alignItems:'center',justifyContent:'center',marginBottom:'5%'}}>
+      <View style={{borderColor:AppColors.azul,borderWidth:1,borderRadius:15}}>
+        <Text style={{textAlign:'center',fontFamily:'inter',fontSize:15,color:'white',margin:10}}>{`For\n${personagem.atributos.forca}`}</Text>
+      </View>
+      <View style={{borderColor:AppColors.azul,borderWidth:1,borderRadius:15}}>
+        <Text style={{textAlign:'center',fontFamily:'inter',fontSize:15,color:'white',margin:10}}>{`Dex\n${personagem.atributos.destreza}`}</Text>
+      </View>
+      <View style={{borderColor:AppColors.azul,borderWidth:1,borderRadius:15}}>
+        <Text style={{textAlign:'center',fontFamily:'inter',fontSize:15,color:'white',margin:10}}>{`Con\n${personagem.atributos.constituicao}`}</Text>
+      </View>
+      <View style={{borderColor:AppColors.azul,borderWidth:1,borderRadius:15}}>
+        <Text style={{textAlign:'center',fontFamily:'inter',fontSize:15,color:'white',margin:10}}>{`Int\n${personagem.atributos.inteligencia}`}</Text>
+      </View>
+      <View style={{borderColor:AppColors.azul,borderWidth:1,borderRadius:15}}>
+        <Text style={{textAlign:'center',fontFamily:'inter',fontSize:15,color:'white',margin:10}}>{`Sab\n${personagem.atributos.sabedoria}`}</Text>
+      </View>
+      <View style={{borderColor:AppColors.azul,borderWidth:1,borderRadius:15}}>
+        <Text style={{textAlign:'center',fontFamily:'inter',fontSize:15,color:'white',margin:10}}>{`Cha\n${personagem.atributos.carisma}`}</Text>
+      </View>
+      
+    </View>
+    <PageButton title ={'Proficiência'} style={{marginVertical:'2%'}}></PageButton>
+    <PageButton title ={'Armas/Magias'} style={{marginVertical:'2%'}}></PageButton>
+    <PageButton title ={'Equipamento'} style={{marginVertical:'2%'}}></PageButton>
+    </View></>}
     </MainView>
 }
